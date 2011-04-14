540f60e98edabdeb89404e548936afce4b1b7fe0
cyberroadie <cyberroadie@gmail.com>
2011-04-14 22:59:29 +0100

    Added groovydoc

--- src/ac/bbk/oodp/elevator/CommandParser.groovy
+++ src/ac/bbk/oodp/elevator/CommandParser.groovy
@@ -1,6 +1,7 @@
 package ac.bbk.oodp.elevator
 
 /**
+ * Parses commands from reader
  * @author Olivier Van Acker, Richard Brown
  * Date: 21/02/2011
  */
@@ -14,11 +15,22 @@ class CommandParser {
     List errorLog
     Command currentCommand
 
+    /**
+     * Constructor
+     * @param reader in put reader for commands
+     */
     CommandParser(BufferedReader reader) {
         this.reader = reader
         currentCommand = CommandFactory.getCommand(reader.readLine())
     }
 
+    /**
+     * Gets the next command available if time is reached
+     * if EOF will return Terminiate command
+     * @param time current time
+     * @return Command, Terminate if no more commands available or
+     * null if no commands are available with current time
+     */
     Command getNextCommand(String time) {
         if (currentCommand.getTime() == time) {
             Command tmpCommand = currentCommand
