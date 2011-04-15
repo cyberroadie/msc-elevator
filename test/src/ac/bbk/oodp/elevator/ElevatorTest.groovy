843fe09a9c9b550b958e5400ae74630d74f3b5d9
Richard Brown <richard.brown1@bbc.co.uk>
2011-04-15 14:35:50 +0100

    Testing more methods

--- test/src/ac/bbk/oodp/elevator/ElevatorTest.groovy
+++ test/src/ac/bbk/oodp/elevator/ElevatorTest.groovy
@@ -7,6 +7,10 @@ class ElevatorTest extends GroovyTestCase {
 
     Elevator testElevator1
 
+    void setUp() {
+        Elevator.callList = []
+    }
+
     void testDecrementWait() {
         this.testElevator1 = new Elevator(1,5)
 
@@ -20,40 +24,40 @@ class ElevatorTest extends GroovyTestCase {
 
         testElevator1.moving = true
         testElevator1.travelTime = 4
-        testElevator1.direction = "up"
+        testElevator1.direction = "UP"
         testElevator1.move()
         assertEquals(5,testElevator1.travelTime)
 
         testElevator1.moving = true
         testElevator1.travelTime = 4
-        testElevator1.direction = "down"
+        testElevator1.direction = "DOWN"
         testElevator1.move()
         assertEquals(3,testElevator1.travelTime)
 
         testElevator1.moving = true
         testElevator1.travelTime = 1
-        testElevator1.direction = "down"
+        testElevator1.direction = "DOWN"
         testElevator1.move()
         assertEquals(0,testElevator1.travelTime)
         assertEquals(5,testElevator1.currentFloor)
 
         testElevator1.moving = true
         testElevator1.travelTime = -9
-        testElevator1.direction = "down"
+        testElevator1.direction = "DOWN"
         testElevator1.move()
         assertEquals(0,testElevator1.travelTime)
         assertEquals(4,testElevator1.currentFloor)
 
         testElevator1.moving = true
         testElevator1.travelTime = 9
-        testElevator1.direction = "up"
+        testElevator1.direction = "UP"
         testElevator1.move()
         assertEquals(0,testElevator1.travelTime)
         assertEquals(5,testElevator1.currentFloor)
 
         testElevator1.moving = false
         testElevator1.travelTime = 4
-        testElevator1.direction = "down"
+        testElevator1.direction = "DOWN"
         testElevator1.move()
         assertEquals(4,testElevator1.travelTime)
     }
@@ -74,6 +78,59 @@ class ElevatorTest extends GroovyTestCase {
         assertEquals(3,testElevator1.travelTime)
     }
 
+    void testDestinationValid() {
+        this.testElevator1 = new Elevator(1,5)
+        Elevator.addCall(CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3"))
+        assertTrue(!this.testElevator1.destinationValid())
+
+        this.testElevator1.respondingCall = Elevator.callList[0]
+        assertTrue(this.testElevator1.destinationValid())
+    }
+
+    void testUpdateDestination() {
+        this.testElevator1 = new Elevator(1,5)
+        Elevator.addCall(CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3"))
+
+        this.testElevator1.updateDestination()
+        assertEquals(2,this.testElevator1.destination)
+
+        setUp()
+        Elevator.addCall(CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3"))
+        Elevator.addCall(CommandFactory.getCommand("call\tBob\t7\t12:30:02\t3"))
+        Elevator.addCall(CommandFactory.getCommand("call\tTrudy\t9\t12:30:02\t3"))
+        testElevator1.moving = true
+        testElevator1.direction = "UP"
+        this.testElevator1.updateDestination()
+        assertEquals(7,this.testElevator1.destination)
+
+        setUp()
+        Elevator.addCall(CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3"))
+        Elevator.addCall(CommandFactory.getCommand("call\tBob\t7\t12:30:02\t3"))
+        Elevator.addCall(CommandFactory.getCommand("call\tTrudy\t9\t12:30:02\t3"))
+        this.testElevator1.currentCalls[0] = Elevator.callList[1]
+        testElevator1.moving = true
+        testElevator1.currentFloor = 1
+        testElevator1.direction = "DOWN"
+        this.testElevator1.updateDestination()
+        assertEquals(3,this.testElevator1.destination)
+    }
+
+    void testChangeFloor() {
+        this.testElevator1 = new Elevator(1,5)
+
+        this.testElevator1.travelTime = 10
+        this.testElevator1.direction = "UP"
+        this.testElevator1.changeFloor()
+        assertEquals(0,this.testElevator1.travelTime)
+        assertEquals(6,this.testElevator1.currentFloor)
+
+        this.testElevator1.travelTime = -10
+        this.testElevator1.direction = "DOWN"
+        this.testElevator1.changeFloor()
+        assertEquals(0,this.testElevator1.travelTime)
+        assertEquals(5,this.testElevator1.currentFloor)
+    }
+
     void testLetPassengerOn() {
         this.testElevator1 = new Elevator(1,5)
 
@@ -89,8 +146,11 @@ class ElevatorTest extends GroovyTestCase {
     void testLetPassengerOff() {
         this.testElevator1 = new Elevator(1,5)
 
+        println Elevator.callList.size()
         Elevator.addCall(CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3"))
+        Elevator.addCall(CommandFactory.getCommand("call\tDave\t2\t12:30:03\t3"))
         testElevator1.currentCalls[0] = Elevator.callList[0]
+        testElevator1.currentCalls[1] = Elevator.callList[1]
         testElevator1.currentFloor = 3
         testElevator1.letPassengerOff()
         assertEquals(0,Elevator.callList.size())
@@ -106,6 +166,7 @@ class ElevatorTest extends GroovyTestCase {
 
     void testCallUpFromFloor() {
         this.testElevator1 = new Elevator(1,5)
+        Elevator.addCall(CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3"))
 
         testElevator1.currentFloor = 1
         assertTrue(testElevator1.callUpFromFloor())
@@ -116,11 +177,18 @@ class ElevatorTest extends GroovyTestCase {
 
     void testCallDownFromFloor() {
         this.testElevator1 = new Elevator(1,5)
+        Elevator.addCall(CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3"))
 
         testElevator1.currentFloor = 7
         assertTrue(testElevator1.callDownFromFloor())
 
         testElevator1.currentFloor = 1
         assertFalse(testElevator1.callDownFromFloor())
+    }
+
+    void testDisplay() {
+        this.testElevator1 = new Elevator(1,5)
+        this.testElevator1.currentCalls[0] = CommandFactory.getCommand("call\tTess\t2\t12:30:02\t3")
+        assertEquals("Elevator Number: 1\nCurrent location: 5\nCurrent passengers: Tess",this.testElevator1.display())
     }
 }
